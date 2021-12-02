d2ecc327b2f0752cdaf9a4df3eefc6a412405205
Karakean <87620247+Karakean@users.noreply.github.com>
2021-12-02 19:18:36 +0100

    Update program.c

--- dot_product/program.c
+++ dot_product/program.c
@@ -3,22 +3,23 @@
 int dot_product(int* arr1, int* arr2, int n);
 int main()
 {
-	int n, tab1[MAX_VECTOR_SIZE], tab2[MAX_VECTOR_SIZE], i;
+	int n, arr1[MAX_VECTOR_SIZE], arr2[MAX_VECTOR_SIZE], i;
 	printf("Enter the size of both vectors: ");
 	scanf_s("%d", &n);
 
 	printf("Enter coordinations of the first vector: \n");
 	for (i = 0; i < n; i++) {
-		scanf_s("%d", &tab1[i]);
+		scanf_s("%d", &arr1[i]);
 	}
 	
 	printf("Enter coordinations of the second vector: \n");
 	for (i = 0; i < n; i++) {
-		scanf_s("%d", &tab2[i]);
+		scanf_s("%d", &arr2[i]);
 	}
 
-	int wynik = dot_product(tab1, tab2, n);
-	printf("The dot product is: %d\n", wynik);
+	int result = dot_product(arr1, arr2, n);
+	printf("The dot product is: %d\n", result);
 
 	return 0;
-}
+}
+
