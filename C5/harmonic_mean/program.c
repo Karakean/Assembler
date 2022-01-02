5e456fa44bbe9d4eab211d34971a40ea2e45d136
Karakean <87620247+Karakean@users.noreply.github.com>
2022-01-02 02:46:32 +0100

    organizing

---
+++ C5/harmonic_mean/program.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+float harmonic_mean(float* arr, unsigned int n);
+int main()
+{
+	float numbers[5] = { -3.0f, 4.5f, 2.1f, -2.1f, -1.5f };
+	float result = harmonic_mean(numbers, 5);
+	printf("%f", result);
+	return 0;
+}
---
+++ programasm.asm
@@ -0,0 +1,10 @@
+.686
+.model flat
+public _harmonic_mean
+.data
+
+.code
+_harmonic_mean PROC
+	ret
+_harmonic_mean ENDP
+END
