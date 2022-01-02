4c0eba48fb22d8f8b1926f6a7638065ccc679aae
Karakean <87620247+Karakean@users.noreply.github.com>
2022-01-02 15:16:25 +0100

    WIP

--- C5/harmonic_mean/program.c
+++ C5/harmonic_mean/program.c
@@ -2,7 +2,7 @@
 float harmonic_mean(float* arr, unsigned int n);
 int main()
 {
-	float numbers[5] = { -3.0f, 4.5f, 2.1f, -2.1f, -1.5f };
+	float numbers[5] = { 3.0f, 4.5f, 2.5f, 2.0f, 1.5f };
 	float result = harmonic_mean(numbers, 5);
 	printf("%f", result);
 	return 0;
--- programasm.asm
+++ programasm.asm
@@ -2,9 +2,33 @@
 .model flat
 public _harmonic_mean
 .data
-
+value dd ?
+nn dd ? ;numbers number
+one dd 1.0
 .code
 _harmonic_mean PROC
+	mov ecx, [esp+8]
+	mov nn, ecx
+	mov ebx, [esp+4] ;array address
+	;fld one ;to reciprocal
+	finit
+	fldz ;beginning sum
+
+	lp: ;loop
+		mov eax, [ebx]
+		mov value, eax
+		fld one
+		fld dword ptr value
+		fdivp st(1), st(0)
+		faddp st(1), st(0)
+		add ebx, 4
+	loop lp
+
+	fild nn
+	fdiv st(0),st(1)
+	fxch
+	fstp st(0)
+
 	ret
 _harmonic_mean ENDP
 END
